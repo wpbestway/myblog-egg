26969c3b93c0cfaf3a7970864b03a9b821bed127
wangpei <1013788632@qq.com>
2020-04-20 19:38:36 +0800

    add admin server

--- server/app/router.js
+++ server/app/router.js
@@ -1,4 +1,3 @@
-'use strict';
 
 /**
  * @param {Egg.Application} app - egg application
@@ -6,13 +5,13 @@
 module.exports = app => {
   const { router, controller } = app
   // client
-  router.get('/client/blog/list', controller.client.blog.getBlogs);
-  router.get('/client/blog/detail', controller.client.blog.getBlog);
-  router.get('/client/tag/list', controller.client.blog.getTags);
-  router.get('/client/classify/list', controller.client.blog.getClassifies);
-  router.get('/client/series/list', controller.client.blog.getSeries);
-  router.get('/client/tool/list', controller.client.blog.getTools);
+  router.get('/client/blog/list', controller.client.blog.getBlogs)
+  router.get('/client/blog/detail', controller.client.blog.getBlog)
+  router.get('/client/tag/list', controller.client.blog.getTags)
+  router.get('/client/classify/list', controller.client.blog.getClassifies)
+  router.get('/client/series/list', controller.client.blog.getSeries)
+  router.get('/client/tool/list', controller.client.blog.getTools)
   router.get('/client/tool/detail', controller.client.blog.getTool)
-  router.get('/client/comment/list', controller.client.blog.getComments);
+  router.get('/client/comment/list', controller.client.blog.getComments)
   router.post('/client/comment/add', controller.client.blog.addComment)
-};
+}
